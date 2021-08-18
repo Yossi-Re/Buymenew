1bdba5e56960b10d5d4a984554ead88b6ea99511
Yossi <y.reuven9@gmail.com>
2021-08-18 14:07:23 +0300

    -updated version

--- src/test/java/Extra.java
+++ src/test/java/Extra.java
@@ -1,2 +1,13 @@
+import com.google.inject.Stage;
+
 public class Extra {
+
+    /**
+     * @param primarystage
+     * @throws Exception
+     */
+    public void start(Stage primarystage) throws Exception {
+
+    }
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -15,22 +15,24 @@ public class MainTest {
 
     public static WebDriver driver;
     private WebDriver chromeDriver;
+    private static WebDriverWait wait;
 
     @BeforeClass
     public static void runOnceBeforeClass() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\ereuv\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
         driver = DriverSingleton.getDriverInstance();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
        driver.get("https://buyme.co.il/");
     }
 
     @Test
     public void assertAllFields() {
-
+//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath())).click();
 IntroRegistrationScreen step1=new IntroRegistrationScreen();
         step1.login();
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@placeholder=\"שם פרטי\"]")));
-        Assert.assertEquals(Constants.NAME, driver.findElement(By.xpath("//*[@placeholder=\"שם פרטי\"]")).getText());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@placeholder='שם פרטי']"))).getText();
+        Assert.assertEquals(Constants.NAME, driver.findElement(By.xpath("//*[@placeholder='שם פרטי']")).getText());
         Assert.assertEquals(Constants.EMAIL, driver.findElement(By.xpath("//*[@id=\"ember1640\"]")).getText());
         Assert.assertEquals(Constants.PASSWORD, driver.findElement(By.xpath("//*[@id=\"valPass\"]")).getText());
         Assert.assertEquals(Constants.PASSWORD, driver.findElement(By.xpath("//*[@id=\"ember1646\"]") ).getText());
